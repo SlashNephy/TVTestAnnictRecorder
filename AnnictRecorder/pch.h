4e9080a12780acf916a3193fd652daaa7890c098
Nep <spica@starry.blue>
2021-06-02 19:41:15 +0900

    vcpkg へのパッケージ管理に移行

--- AnnictRecorder/pch.h
+++ AnnictRecorder/pch.h
@@ -10,9 +10,6 @@
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 
-// error C4996: '_Header_cstdbool': warning STL4004: <ccomplex>, <cstdalign>, <cstdbool>, and <ctgmath> are deprecated in C++17. You can define _SILENCE_CXX17_C_HEADER_DEPRECATION_WARNING or _SILENCE_ALL_CXX17_DEPRECATION_WARNINGS to acknowledge that you have received this warning.
-#define _SILENCE_CXX17_C_HEADER_DEPRECATION_WARNING
-
 #include <combaseapi.h>
 #include <cstdint>
 #include <cstdlib>
@@ -27,21 +24,13 @@
 #include <Shlwapi.h>
 #pragma comment(lib, "shlwapi.lib")
 
-#include "nlohmann/json.hpp"
-
 #include "cpr/cpr.h"
-#ifdef _DEBUG
-#pragma comment(lib, "cprd.lib")
-#else
 #pragma comment(lib, "cpr.lib")
-#endif
 
 #include "yaml-cpp/yaml.h"
-#ifdef _DEBUG
-#pragma comment(lib, "yaml-cppd.lib")
-#else
 #pragma comment(lib, "yaml-cpp.lib")
-#endif
+
+#include "nlohmann/json.hpp"
 
 #include "pugixml.hpp"
 #ifdef _DEBUG
