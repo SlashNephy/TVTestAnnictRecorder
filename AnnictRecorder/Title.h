76ac0506bd38d2e23d69f2dc3dbb6c8bc3061403
Nep <spica@starry.blue>
2021-06-18 19:27:09 +0900

    [無] で始まる AT-X の番組の記録を行えるようにした

--- AnnictRecorder/Title.h
+++ AnnictRecorder/Title.h
@@ -14,7 +14,7 @@ namespace Title
 
     static AtxTitleResult ExtractAtxTitle(const std::wstring& eventName)
     {
-        const auto multipleEpisodeRegex = std::wregex(LR"(^(.+)\s#([\d\.]+)[-・]#([\d\.]+).*$)");
+        const auto multipleEpisodeRegex = std::wregex(LR"(^(?:\[無\])?(.+)\s#([\d\.]+)[-・]#([\d\.]+).*$)");
         if (std::wsmatch match; std::regex_match(eventName, match, multipleEpisodeRegex))
         {
             return {
@@ -25,7 +25,7 @@ namespace Title
             };
         }
 
-        const auto singleEpisodeRegex = std::wregex(LR"(^(.+)\s#([\d\.]+).*$)");
+        const auto singleEpisodeRegex = std::wregex(LR"(^(?:\[無\])?(.+)\s#([\d\.]+).*$)");
         if (std::wsmatch match; std::regex_match(eventName, match, singleEpisodeRegex))
         {
             const auto start = static_cast<float_t>(_wtof(match[2].str().c_str()));
